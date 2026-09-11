bd0df637b7582639821c7a6bda319d8a64bc25ac
Nathnael Getachew <nathangetachew536@gmail.com>
2026-09-11 13:57:09 +0300

    Implement chunk management and debugging functionality

---
+++ src/main.c
@@ -0,0 +1,19 @@
+#include "chunk.h"
+#include "common.h"
+#include "debug.h"
+#include <stdio.h>
+
+int main(int argc, const char *argv) {
+    Chunk chunk;
+    initChunk(&chunk);
+
+    int constant = addConstant(&chunk, 1.2);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+
+    writeChunk(&chunk, OP_RETURN, 1);
+    disassembleChunk(&chunk, "test chunk");
+    freeChunk(&chunk);
+
+    return 0;
+}
