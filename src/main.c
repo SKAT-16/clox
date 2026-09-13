ee61bf3409c2f4673a53920be9d00749394917be
Nathnael Getachew <nathangetachew536@gmail.com>
2026-09-13 10:31:06 +0300

    Implement basic arithmetic operations and VM structure

--- src/main.c
+++ src/main.c
@@ -1,18 +1,37 @@
 #include "chunk.h"
 #include "common.h"
 #include "debug.h"
+#include "vm.h"
 #include <stdio.h>
 
 int main(int argc, const char *argv) {
+    initVM();
+
     Chunk chunk;
     initChunk(&chunk);
 
-    int constant = addConstant(&chunk, 1.2);
-    writeChunk(&chunk, OP_CONSTANT, 1);
-    writeChunk(&chunk, constant, 1);
+    int constant = addConstant(&chunk, 123);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
 
-    writeChunk(&chunk, OP_RETURN, 1);
-    disassembleChunk(&chunk, "test chunk");
+    constant = addConstant(&chunk, 3.4);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_ADD, 123);
+
+    constant = addConstant(&chunk, 5.6);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_DIVIDE, 123);
+
+    writeChunk(&chunk, OP_NEGATE, 123);
+    writeChunk(&chunk, OP_RETURN, 123);
+
+    interpret(&chunk);
+
+    freeVM();
     freeChunk(&chunk);
 
     return 0;
